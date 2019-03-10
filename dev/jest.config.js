a7176474f2e445f2d170bc8c855e5d752968ff64
Michael Borde <michael.borde@ubimail.fr>
2019-03-10 12:34:11 +0100

    initial import

---
+++ dev/jest.config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  rootDir: '../',
+  testRegex: '/lib/.*\\.spec\\.js$',
+  moduleFileExtensions: ['js', 'json'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/out/']
+};
